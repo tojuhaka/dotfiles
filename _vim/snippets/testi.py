5b9cf20d71c008dc748054d7d0bb5a0badb8a40f
Toni Haka-Risku <tojuhaka@localhost.localdomain>
2012-11-16 17:17:36 +0200

    extra files removed

---
+++ _vim/snippets/testi.py
@@ -0,0 +1,13 @@
+def fname():
+    """docstring for fname"""
+    # TODO: write code...
+
+class aras(asdasd):
+    """asda"""
+    def __init__(self, asdas):
+        asdasd
+        self.as = as
+        adsad       ada
+
+testi.t:    q
+:
